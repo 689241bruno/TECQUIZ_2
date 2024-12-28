6c3bf79f6910c8d6ec6c1669462fbacb436d389a
Bruno Santos <66684921+689241bruno@users.noreply.github.com>
2024-12-28 15:28:08 -0300

    deixando responsivo pra celular

--- pergunta1.js
+++ pergunta1.js
@@ -1,18 +1,29 @@
-const resp = document.getElementById("respCerta");
-const btn = document.getElementById("btnConfirmar");
+const respCerta = document.getElementById("respCerta");
+const errada1 = document.getElementById("q1");
+const errada2 = document.getElementById("q2");
+const errada3 = document.getElementById("q3");
+const boxResp = document.getElementById("box-resp");
 
 
-resp.addEventListener("click", function(){
-    const cont = 1;
-    const resposta = false;
-    cont ++;
-    if(cont % 2 == 0){
-        resposta = false;
-    } else{
-        resposta = true;
-    }
+
+respCerta.addEventListener("click", function(){
+    boxResp.classList.add("box-resp");
+    boxResp.style.backgroundColor = "#ff0000";
+    setTimeout(() => {
+        boxResp.classList.remove("box-resp");
+        boxResp.classList.add("box-resp-sumir");
+    }, 2500);
 });
 
-btn.addEventListener("click", function(){
-    alert("ola");
+errada1.addEventListener("click", function(){
+    boxResp.classList.add("box-resp");
+    boxResp.style.backgroundColor = "#FFF";
+});
+errada2.addEventListener("click", function(){
+    boxResp.classList.add("box-resp");
+    boxResp.style.backgroundColor = "#FFF";
+});
+errada3.addEventListener("click", function(){
+    boxResp.classList.add("box-resp");
+    boxResp.style.backgroundColor = "#FFF";
 });
